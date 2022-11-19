aee397b3c3211937dd3f17b9262ecdd4c3cec8e5
Anthony Kakatera <anthonyckakatera@gmail.com>
2022-11-20 01:57:12 +0200

    RestTemplate Post data to URL feature

--- src/main/java/com/resttemplate/client/rest/EntryController.java
+++ src/main/java/com/resttemplate/client/rest/EntryController.java
@@ -1,7 +1,13 @@
 package com.resttemplate.client.rest;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.resttemplate.client.dto.AnimalsDTO;
 import com.resttemplate.client.dto.CatFactDTO;
 import com.resttemplate.client.dto.UniversitiesDTO;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -52,7 +58,11 @@ public class EntryController {
         //Arrays.stream(universitiesDTO).forEach(n -> System.out.println(n.getName()));
 
         for(UniversitiesDTO universityDTO : universitiesDTO){
-            loopingContent += "<tr><td>"+universityDTO.getName()+"</td><td>"+universityDTO.getProvince()+"</td><td>"+Arrays.toString(universityDTO.getWeb_pages()).replace("[", "").replace("]", "")+"</td><td>"+Arrays.toString(universityDTO.getDomains()).replace("[", "").replace("]", "")+"</td><td>"+universityDTO.getAlpha_two_code()+"</td></tr>";
+            loopingContent += "<tr><td>"+universityDTO.getName()+"</td><td>"
+                    +universityDTO.getProvince()+"</td><td>"
+                    +Arrays.toString(universityDTO.getWeb_pages()).replace("[", "").replace("]", "")+"</td><td>"
+                    +Arrays.toString(universityDTO.getDomains()).replace("[", "").replace("]", "")+"</td><td>"
+                    +universityDTO.getAlpha_two_code()+"</td></tr>";
         }
 
         //building simple out put
@@ -68,5 +78,35 @@ public class EntryController {
                 "</table>";
 
         return table;
+    }
+
+    @RequestMapping("/createUser")
+    @ResponseBody
+    private String postUser(){
+
+        String uri = "https://reqres.in/api/users";
+
+        //Rest template
+        RestTemplate restTemplate = new RestTemplate();
+
+        //header
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        //json object
+        JSONObject jsonObject = new JSONObject();
+
+        //add to json object
+        jsonObject.put("name", "Jonathan Heroes");
+        jsonObject.put("job", "Leader");
+
+        //Http entity to send request
+        HttpEntity<String> request =
+                new HttpEntity<String>(jsonObject.toString(), headers);
+
+        //sending request and getting response entity
+        ResponseEntity<String> response = restTemplate.postForEntity( uri, request , String.class );
+
+        return "User creation page, response : " + response.getStatusCodeValue();
     }
 }
